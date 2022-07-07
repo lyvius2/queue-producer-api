9b3a83e94cc920f50245e06c3d5e3049e55e1d8d
walter.hwang <lyvius2@naver.com>
2022-07-07 23:04:26 +0900

    init commit

---
+++ src/main/java/com/walter/queue/producer/QueueProducerApiApplication.java
@@ -0,0 +1,13 @@
+package com.walter.queue.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueProducerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueProducerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walter/queue/producer/QueueProducerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walter.queue.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueProducerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
